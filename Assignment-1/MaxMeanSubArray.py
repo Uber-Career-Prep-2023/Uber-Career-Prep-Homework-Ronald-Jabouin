9262d68a7d3c7bc6f62e7d4fef857f7285ed6113
Ronald Jabouin <37760008+ronjj@users.noreply.github.com>
2023-02-05 08:41:14 -0500

    first commit

---
+++ Assignment-1/MaxMeanSubArray.py
@@ -0,0 +1,23 @@
+
+"""
+Test Inputs and Outputs:
+
+Array: [4,5,-3,2,6,1]
+k = 2
+Output: 4.5
+
+Array: [4,5,-3,2,6,1]
+k = 2
+Output:3
+
+Array: [1,1,1,1,-1,-1,2,-1,-1,6]
+k = 3
+Output: 1
+
+Array: [1,1,1,1,-1,-1,2,-1,-1,6]
+k = 4
+Output: 1.5
+"""
+
+def MaxMeanSubArray(arr, k):
+    pass

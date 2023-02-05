6e201f47389f1d0cdbc48f584b783f5a4fe8b026
Ronald Jabouin <37760008+ronjj@users.noreply.github.com>
2023-02-05 09:27:19 -0500

    question 1

--- Assignment-1/MaxMeanSubArray.py
+++ Assignment-1/MaxMeanSubArray.py
@@ -1,4 +1,10 @@
 
+# Technique: Fixed Sliding Window
+# Time Complexity
+# Time Spent: 40 minutes
+
+
+
 """
 Test Inputs and Outputs:
 
@@ -7,8 +13,8 @@ k = 2
 Output: 4.5
 
 Array: [4,5,-3,2,6,1]
-k = 2
-Output:3
+k = 3
+Output: 3
 
 Array: [1,1,1,1,-1,-1,2,-1,-1,6]
 k = 3
@@ -19,5 +25,44 @@ k = 4
 Output: 1.5
 """
 
+# Notes and Thoughts
+
+# Start at 0 and have 2nd pointer be k-1
+# Mean = element + element ... / k
+
+
 def MaxMeanSubArray(arr, k):
-    pass
+    #variable to update as new values are computed
+    largestMean = 0
+    
+    # Pointer starting at 0
+    for x in arr:
+        print(f"x is {x}")
+
+        #if x is not the last position that allows window size to fit in array
+        if x is not arr[-k]:
+            # FIXME: the x here is not the index, it's the exact value. 
+            secondPointer = arr[x+(k-1)]
+            print(arr[x])
+            print(f"x is {x} and secondPointer is {secondPointer}")
+            print(arr[x:secondPointer])
+            print("--------")
+            meanSum = sum(arr[x:secondPointer])
+            mean = meanSum / k
+
+            if mean > largestMean:
+                largestMean = mean
+    
+    # #Pointer starting at k-1
+    #     for y in arr[k-1:]:
+    #         # print(x+y)
+    #         mean = x + y / k
+    #         if mean > largestMean:
+    #             largestMean = mean
+    
+    return largestMean
+
+#Reflection:
+    # Realized my main issue towards the end while debugging.
+    # I was incorrectly calculating indexes and instead getting exact values which
+    # threw off my calucations. Example in FIXME comment.
